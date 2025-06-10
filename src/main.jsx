278d0c9b8a089192e30c7cf44aa5ef6435f29590
Sai vara prasad Avula <saivaraprasadavula@Mac.lan>
2025-06-09 22:59:01 -0400

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // <--- Add this import
+import { BrowserRouter } from 'react-router-dom'; 
 import './index.css';
 import App from './App.jsx';
 
 console.log("Main.jsx loaded");
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* <--- Wrap App with BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    root.render(
+      <BrowserRouter basename="/boardbid-ui">
+        <App />
+      </BrowserRouter>
+    );
   </StrictMode>,
 );
+
+
+
