f7afcf41242f77bb884b871cfdd3ac2ff43a2401
Sai vara prasad Avula <saivaraprasadavula@Mac.lan>
2025-06-09 23:01:52 -0400

    Fix main.jsx router basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
 console.log("Main.jsx loaded");
-createRoot(document.getElementById('root')).render(
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    root.render(
-      <BrowserRouter basename="/boardbid-ui">
-        <App />
-      </BrowserRouter>
-    );
-  </StrictMode>,
+    <BrowserRouter basename="/boardbid-ui">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
-
-
-
