6b32daab088539ab3ec70f75efed3982ec65581e
Sai Vara Prasad Avula <54263491+saivaraprasad-avula@users.noreply.github.com>
2025-08-04 07:36:17 +0530

    Replace new campaign page with Airtable form

--- src/pages/NewCampaignPage.jsx
+++ src/pages/NewCampaignPage.jsx
@@ -1,11 +1,16 @@
 import InternalLayout from '../layout/InternalLayout';
-import NewCampaign from '../components/NewCampaign';
 
 export default function NewCampaignPage() {
   return (
     <InternalLayout>
       <div className="max-w-5xl mx-auto w-full">
-        <NewCampaign />
+        <iframe
+          src="https://airtable.com/embed/appq8sWGxB7KeB9EQ/pag0MpfZJx4fS4mMY/form"
+          className="airtable-embed airtable-dynamic-height w-full"
+          frameBorder="0"
+          onWheel=""
+          style={{ background: 'transparent', border: '1px solid #ccc', height: '2000px' }}
+        />
       </div>
     </InternalLayout>
   );
