c13fbdab28f31a5c6d3cfc2241019e20a591ed47
Sai Vara Prasad Avula <54263491+saivaraprasad-avula@users.noreply.github.com>
2025-08-05 10:55:55 +0530

    feat: add DOOH campaign request form

--- src/pages/NewCampaignPage.jsx
+++ src/pages/NewCampaignPage.jsx
@@ -1,16 +1,11 @@
 import InternalLayout from "../layout/InternalLayout";
+import DoohCampaignRequestForm from "../components/DoohCampaignRequestForm";
 
 export default function NewCampaignPage() {
   return (
     <InternalLayout>
       <div className="max-w-5xl mx-auto w-full">
-        <iframe
-          src="https://airtable.com/embed/appq8sWGxB7KeB9EQ/pag0MpfZJx4fS4mMY/form"
-          className="airtable-embed airtable-dynamic-height w-full"
-          frameBorder="0"
-          onWheel=""
-          style={{ background: "transparent", border: "1px solid #ccc", height: '1300px' }}
-        />
+        <DoohCampaignRequestForm />
       </div>
     </InternalLayout>
   );
