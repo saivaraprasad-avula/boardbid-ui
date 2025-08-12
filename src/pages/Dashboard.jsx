fb3a187989dcb17a23a5e6313639dfe925cedcb9
Sai Vara Prasad Avula <54263491+saivaraprasad-avula@users.noreply.github.com>
2025-08-13 02:07:52 +0530

    Add Get Started card to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,15 @@
 import InternalLayout from '../layout/InternalLayout';
 import WelcomeBack from '../components/WelcomeBack';
+import GetStartedCard from '../components/GetStartedCard';
 
 export default function Dashboard() {
   return (
     <InternalLayout>
-      <div className="scale-50 origin-top-left w-fit">
-
+      <div className="space-y-6">
+        <WelcomeBack />
+        <GetStartedCard />
       </div>
     </InternalLayout>
   );
 }
+
